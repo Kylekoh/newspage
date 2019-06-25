2f71710dee42f1d8cefb6bb801ddcb04d782af5a
고영일 <kyle_koh@goyeong-il-ui-MacBook-Pro.local>
2019-06-25 18:58:26 +0900

    Updates

---
+++ precache-manifest.c97b0e6c18a3bb5621c43aa432e90085.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9099c9d983ca804630559dd51b6080ac",
+    "url": "/newspage/index.html"
+  },
+  {
+    "revision": "5eacc9b9b0cf2231ff2f",
+    "url": "/newspage/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "a7b8ff734348789a4a7e",
+    "url": "/newspage/static/css/main.c9c6526c.chunk.css"
+  },
+  {
+    "revision": "5eacc9b9b0cf2231ff2f",
+    "url": "/newspage/static/js/2.7134d852.chunk.js"
+  },
+  {
+    "revision": "a7b8ff734348789a4a7e",
+    "url": "/newspage/static/js/main.1c0da716.chunk.js"
+  },
+  {
+    "revision": "1bc747675c7bf648f0af",
+    "url": "/newspage/static/js/runtime~main.c6600d38.js"
+  },
+  {
+    "revision": "301248ffce34e0062ad9676aa7499aa1",
+    "url": "/newspage/static/media/image1.301248ff.png"
+  },
+  {
+    "revision": "d1e2f91e26f97590cbb89e18e1bc9a0a",
+    "url": "/newspage/static/media/image2.d1e2f91e.jpg"
+  },
+  {
+    "revision": "4a6de3dd3038ad362d60274dc699dcfc",
+    "url": "/newspage/static/media/image3.4a6de3dd.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/newspage/static/media/logo.5d5d9eef.svg"
+  }
+]);
