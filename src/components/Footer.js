44f4189bd79757452a3f507cf87396261f3bdadb
고영일 <kyle_koh@goyeong-il-ui-MacBook-Pro.local>
2019-06-25 16:12:55 +0900

    second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,38 @@ import './Footer.css';
 class Footer extends Component {
     render() {
         return (
-                <div className="gift_wrapper">
-                    뉴스 IT
+                <div className="footer_wrapper">
+                    <div className="footer_header">
+                        <div className="head_item">뉴스</div>
+                        <div className="head_item">연예</div>
+                        <div className="head_item">스포츠</div>
+                    </div>
+                    <div className="foot_main">
+                        <div className="foot_main_item">홈</div>
+                        <div className="foot_main_item">사회</div>
+                        <div className="foot_main_item">정치</div>
+                        <div className="foot_main_item">연예홈</div>
+                        <div className="foot_main_item">최신뉴스</div>
+                        <div className="foot_main_item">경제</div>
+                        <div className="foot_main_item">국제</div>
+                        <div className="foot_main_item">문화</div>
+                        <div className="foot_main_item">Top 키워드</div>
+                        <div className="foot_main_item">많이본뉴스</div>
+                        <div className="foot_main_item">IT</div>
+                        <div className="foot_main_item">연재</div>
+                        <div className="foot_main_item">랭킹</div>
+                        <div className="foot_main_item">스타톡</div>
+                        <div className="foot_main_item">TV</div>
+                        <div className="foot_main_item">이슈</div>
+                        <div className="foot_main_item">배열이력</div>
+                        <div className="foot_main_item">라디오</div>
+                        <div className="foot_main_item">포토</div>
+                        <div className="foot_main_item">투표</div>
+                        <div className="foot_main_item">미디어랩</div>
+                        <div className="foot_main_item">언론사별</div>
+                        <div className="foot_main_item">전체기사</div>
+                        <div className="foot_main_item">사진전</div>
+                    </div>
                 </div>
         );
     }
